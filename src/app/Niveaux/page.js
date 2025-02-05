54d296ae8ea56853694899710d680ff805c1e6af
abdou <abdouthomes20048@gmail.com>
2025-02-05 22:02:23 +0100

    ...

--- src/app/Niveaux/page.js
+++ src/app/Niveaux/page.js
@@ -1,12 +1,31 @@
 import React from "react";
-import BoxsNevaux from "./Componet/BoxNevaux/BoxsNevaux";
+import BoxNevaux from "./Componet/BoxNevaux/BoxNevaux";
 
 const page = () => {
   // the proplem Her is The "."
   return (
-    <>
-      <BoxsNevaux />
-    </>
+    <div className="px-10 py-11 grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 gap-4 lg:grid-cols-4 text-end">
+      <BoxNevaux
+        image={require("@/public/Svg_NumberNeavaux/level1.png")}
+        textNevaux={"السنة الأولى متوسط"}
+        hrefLink={"1am"}
+      />{" "}
+      <BoxNevaux
+        image={require("@/public/Svg_NumberNeavaux/level2.png")}
+        textNevaux={"السنة الثانية متوسط"}
+        hrefLink={"2am"}
+      />{" "}
+      <BoxNevaux
+        image={require("@/public/Svg_NumberNeavaux/level3.png")}
+        textNevaux={"السنة الثالثة متوسط"}
+        hrefLink={"3am"}
+      />{" "}
+      <BoxNevaux
+        image={require("@/public/Svg_NumberNeavaux/level4.png")}
+        textNevaux={"السنة الرابعة متوسط"}
+        hrefLink={"4am"}
+      />
+    </div>
   );
 };
 
